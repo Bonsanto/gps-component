70a4ee094fdc14722db419da765c0a646fa1f215
Bonsanto <xbonsx@gmail.com>
2015-03-06 22:41:31 -0430

    - First working version

---
+++ dist/gpscomp.js
@@ -0,0 +1,35 @@
+var GpsProto = Object.create(HTMLDivElement.prototype);
+
+GpsProto.createdCallback = function () {
+	var shadow = this.createShadowRoot();
+	var map = document.createElement("img");
+
+	if (geoAvailable()) {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			var lati = position.coords.latitude;
+			var long = position.coords.longitude;
+			var acc = position.coords.accuracy;
+			var mark = "&markers=color:blue%7Clabel:S%7C" + lati + "," + long;
+
+			map.src = "http://maps.googleapis.com/maps/api/staticmap?center=" +
+			mark + "&zoom=" + lati + "," + "12&size=400x400&sensor=false&markers=" + long;
+
+			shadow.appendChild(map);
+		}, function (error) {
+			alert(error);
+		}, {
+			enableHighAccuracy: true,
+			timeout: 5000,
+			maximumAge: 60000
+		});
+	}
+};
+
+
+var geoAvailable = function () {
+	return navigator.geolocation !== undefined;
+};
+
+GPS = document.registerElement('x-map', {
+	prototype: GpsProto
+});
